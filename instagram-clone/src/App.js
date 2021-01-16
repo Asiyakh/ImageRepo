ea611c817f542cc0845b4165da35431b35da82cc
AsiyaKhan <asiya00khan@gmail.com>
2021-01-15 20:44:01 -0500

    background for webapp

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://img.icons8.com/bubbles/2x/camera.png"
+          alt=""
+        />
+      
+      </div>
+
+      <h1>Welcome to Photobooth</h1>
+
+    </div>
+  );
+}
+
+export default App;
